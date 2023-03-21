b4fb57fb6194518a63f4b8a5fc04293e549b38a3
김수연 <@DESKTOP-VL16R2I>
2023-03-21 13:32:46 +0900

    main_v3

--- main.php
+++ main.php
@@ -7,6 +7,13 @@
     <title>FLASHBACK</title>
 </head>
 <body>
-
+    <div style="width:97%;height:80px;margin:20px">
+        <form name="상단바" action="다음페이지로" method="post">
+            <input type="button" name="아이디값" value="My Gallery">
+            <input type="button" name="아이디값" value="Share Gallery">
+            <input type="button" name="아이디값" value="Upload">
+            <input type="button" name="아이디값" value="Account">
+        </form>
+    </div>
 </body>
 </html>
