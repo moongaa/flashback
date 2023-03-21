dc840634a0cea05a2c7ca6da353d0278e99dbb88
김수연 <@DESKTOP-VL16R2I>
2023-03-22 01:45:38 +0900

    main_v5

--- main.php
+++ main.php
@@ -7,17 +7,31 @@
     <title>FLASHBACK</title>
 </head>
 <body>
-    <div style="height:80px;margin:20px">
-        <form name="상단바" action="다음페이지로" method="post">
-            <input type="button" name="아이디값" value="FlashBack">
-            <input type="button" name="아이디값" value="My Gallery">
-            <input type="button" name="아이디값" value="Share Gallery">
-            <input type="button" name="아이디값" value="Upload">
-            <input type="button" name="아이디값" value="Account">
-        </form>
-    </div>
-    <div>
-        <IMG SRC="./빛나는밤.jpg" Align=center height="">
+    <link rel="stylesheet" href="css/main.css">
+    <div class="container">
+        <div class="header">
+            <form name="상단바" action="메인페이지로" method="post">
+                <input class="Flashback" type="button" name="아이디값" value="FlashBack">
+            </form>
+            <form name="상단바" action="마이갤러리로" method="post">
+                <input class="top-button" type="button" name="아이디값" value="My Gallery">
+            </form>
+            <form name="상단바" action="공유갤러리로" method="post">
+                <input class="top-button" type="button" name="아이디값" value="Share Gallery">
+            </form>
+            <form name="상단바" action="업로드 창띄우기" method="post">
+                <input class="top-button" type="button" name="아이디값" value="Upload">
+            </form>
+            <form name="상단바" action="계정 페이지" method="post">
+                <input class="top-button" type="button" name="아이디값" value="Account">
+            </form>
+        </div>
+        <div class="content">
+            <IMG class="image" src="img/빛나는밤.jpg" width="60%" alt="My Image">
+        </div>
+        <div class="footer">
+            더보기
+        </div>
     </div>
 </body>
 </html>
