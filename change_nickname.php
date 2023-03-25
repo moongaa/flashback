b883d6bf01c08baa020ccdf0602f18eba143c5ce
moonga <lilymoon0114@gmail.com>
2023-03-25 14:56:19 +0900

    account v4

---
+++ change_nickname.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $current_nickname = $_POST["current_nickname"];
+  $new_nickname = $_POST["new_nickname"];
+
+
+  $sql = "UPDATE users SET nickname='$new_nickname' WHERE nickname='$current_nickname'";
+  $result = mysqli_query($conn, $sql);
+
+
+  if ($result) {
+    echo "닉네임이 변경되었습니다.";
+  } else {
+    echo "닉네임 변경에 실패했습니다.";
+  }
+}
+
+mysqli_close($conn);
+?>
