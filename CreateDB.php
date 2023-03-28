620303646516669f2d44d9261bfedd77f6ff5fa2
김수연 <@DESKTOP-VL16R2I>
2023-03-28 14:52:14 +0900

    DB_v2

--- CreateDB.php
+++ CreateDB.php
@@ -1,27 +1,27 @@
 <?php
     include "ConnectDB.php";
-    $sql_account = "create table account()";
+    $sql_account = "create table account(num int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(10) NOT NULL, id varchar(10) NOT NULL, pw varchar(20) NOT NULL, introduction text)";
     $res_account = mysqli_query($connect, $sql_account);
-    if($res_b){
+    if($res_account){
       echo "테이블 생성<br>";
     }
     else {
       echo "fff<br>";
     }
 
-    $sql_gallery = "create table gallery(name varchar(20), stu_num int, id varchar(20), password varchar(50), contents text, diary text, primary key(name), key(stu_num))";
+    $sql_gallery = "Create table gallery(name varchar(10) NOT NULL, title varchar(20) NOT NULL, contents text, image MEDIUMBLOB, data timestamp NOT NULL)";
     $res_gallery = mysqli_query($connect, $sql_gallery);
-    if($res_r){
-      echo "테이블 생성";
+    if($res_gallery){
+      echo "테이블 생성<br>";
     }
     else {
-      echo "ffff";
+      echo "ffff<br>";
     }
 
     $sql = "ALTER table account default character set utf8 collate utf8_general_ci";
     $res = mysqli_query($connect, $sql);
     if($res){
-      echo "글씨체 바꾸기 성공(아마?)";
+      echo "글씨체 바꾸기 성공(아마?)<br>";
     }
     else {
       echo "ffff";
@@ -29,7 +29,7 @@
     $sql = "ALTER table gallery default character set utf8 collate utf8_general_ci";
     $res = mysqli_query($connect, $sql);
     if($res){
-      echo "글씨체 바꾸기 성공(아마?)";
+      echo "글씨체 바꾸기 성공(아마?)<br>";
     }
     else {
       echo "ffff";
